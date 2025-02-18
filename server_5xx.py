34efa2953258773a0fd35cde6b34d223ce526b0d
yuchenluamazon <105088379+yuchenluamazon@users.noreply.github.com>
2025-02-18 11:36:02 -0800

    Update server_5xx.py

--- server_5xx.py
+++ server_5xx.py
@@ -5,21 +5,16 @@ import os
 import random
 
 def hello_world(request):
-    # Check if the 'error' query parameter is present
-    if 'error' in request.params:
-        # You can use random.random() to introduce randomness in error generation
-        if random.random() < 0.5:  # 50% chance of generating an error
-            return Response(status_code=500, body="Internal Server Error")
+    if random.random() < 0.5:  # 50% chance of generating an error
+        return Response(status_code=500, body="Internal Server Error")
     
     # Normal response
-    name = os.environ.get('NAME')
-    if name == None or len(name) == 0:
-        name = "world"
-    message = "GM all, " + name + "!\n"
+    name = os.environ.get('NAME', 'world')
+    message = f"GM all, {name}!\n"
     return Response(message)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT"))
+    port = int(os.environ.get("PORT", 8080))
     with Configurator() as config:
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
