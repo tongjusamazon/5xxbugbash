2195ec120dd731010f3717093aa27dc805539b69
yuchenluamazon <105088379+yuchenluamazon@users.noreply.github.com>
2025-02-25 22:28:07 -0800

    Update server_GET.py

--- server_GET.py
+++ server_GET.py
@@ -7,13 +7,13 @@ def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "Hello, " + name + "!\n"
+    message = "GM all, " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
     with Configurator() as config:
-        config.add_route('hello', '/', request_method='GET')
+        config.add_route('hello', '/',request_method='GET')
         config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
