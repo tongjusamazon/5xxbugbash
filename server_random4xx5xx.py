ee4c4142045f982d9b61cdac1303294acd24363a
yuchenluamazon <105088379+yuchenluamazon@users.noreply.github.com>
2025-02-25 23:16:12 -0800

    Create server_random4xx5xx.py

---
+++ server_random4xx5xx.py
@@ -0,0 +1,28 @@
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+import os
+import random
+
+def hello_world(request):
+    chance = random.random()
+    if chance < 0.3:  # 30% chance of generating a 5xx error
+        return Response(status_code=500, body="500 Internal Server Error, thrown on purpose")
+    elif chance < 0.6:  # 30% chance of generating a 4xx error
+        error_codes = [400, 401, 403, 404, 429]
+        status_code = random.choice(error_codes)
+        return Response(status_code=status_code, body=f"{status_code} error, thrown on purpose")
+    
+    # Normal response
+    name = os.environ.get('NAME', 'world')
+    message = f"GM all, {name}!\n"
+    return Response(message)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()
